a935c41f1462a7fd3fb0d2ec5778edf8e94805f4
toilahuy0109 <huymax0109@gmail.com>
2025-07-04 12:34:44 +0700

    new

--- install/drone_drive/include/drone_drive/drone_drive/drone_drive.hpp
+++ install/drone_drive/include/drone_drive/drone_drive/drone_drive.hpp
@@ -8,5 +8,45 @@
 #include <rclcpp/rclcpp.hpp>
 
 
+namespace gazebo
+{
+    class DroneDiffDrivePlugin : public ModelPlugin
+    {
+        public:
+        DroneDiffDrivePlugin();
+        void Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf) override;
+
+        protected:
+        void OnUpdate();
+
+        private:
+        
+        gazebo::physics::ModelPtr model_;
+        gazebo::physics::LinkPtr base_link_;
+        gazebo::event::ConnectionPtr update_connection_;
+
+        //ROS components 
+        gazebo_ros::Node::SharedPtr ros_node_;
+        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_sub_;
+        std::string cmd_topic_;
+
+        //Parameters
+
+        double k_thrust_;
+        double k_yaw_;
+        double k_forward_;
+        double max_rotor_vel_;
+        double arm_length_;
+
+        //Control inputs
+
+        double linear_x_;
+        double linear_z_;
+        double angular_z_;
+
+        void OnCmdVel(const geometry_msgs::msg::Twist::SharedPtr msg);
+    };
+}
+
 
 #endif
--- src/drone_drive/src/drone_drive.cpp
+++ src/drone_drive/src/drone_drive.cpp
@@ -1,12 +1,36 @@
-#ifndef DIFF_DRIVE_CONTROLLER__DIFF_DRIVE_CONTROLLER_HPP_
-#define DIFF_DRIVE_CONTROLLER__DIFF_DRIVE_CONTROLLER_HPP_
-
-#include <chrono>
+#include <drone_drive/drone_drive.hpp>
+#include <gazebo/common/Events.hh>
+#include <ignition/math/Vector3.hh>
 #include <cmath>
-#include <memory>
-#include <queue>
-#include <string>
-#include <vector>
 
+namespace gazebo{
+    GZ_REGISTER_MODEL_PLUGIN(DroneDiffDrivePlugin)
 
-#endif
+    DroneDiffDrivePlugin::DroneDiffDrivePlugin()
+        :   linear_x_(0.0), linear_z_(0.0), angular_z_(0.0),
+            k_thrust_(0.0005), k_yaw_(0.0001), k_forward_(0.5),
+            max_rotor_vel_(100.0),arm_length_(0.4){}
+  
+    void DroneDiffDrivePlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf)
+    {
+        model_ = model;
+
+        // Get base_link
+
+        base_link_ = model_->GetLink("base_link");
+        if(!base_link_){
+            gzerr << "ERROR: Link 'base_link' not found in model \n";
+            return;
+        }
+
+        //Load parameters from SDF
+
+        if(sdf->HasElement("robot_namespace")){
+            cmd_topic_ = sdf->Get<std::string>("robot_namespace") + "/diff_drive";
+        }else{
+            cmd_topic_ = "/drone/diff_drive";
+        }
+
+        
+    }
+}
